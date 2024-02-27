b1140bf1ca15c967e54fc712588864ba6971aa22
Veng-Eang <oengvengeang@gmail.com>
2024-02-26 22:03:41 -0500

    Loading meals data

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/categories_screen.dart';
 
 final theme = ThemeData(
+  useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(255, 131, 57, 0),
     brightness: Brightness.dark,
--- lib/widgets/categories_item.dart
+++ lib/widgets/categories_item.dart
@@ -9,10 +9,12 @@ class CategoryItem extends StatelessWidget {
     return InkWell(
       onTap: () {},
       splashColor: Theme.of(context).primaryColor,
+      // splashColor: Colors.amber,
       borderRadius: BorderRadius.circular(16),
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
           gradient: LinearGradient(
             colors: [
               category.color.withOpacity(0.55),
