a07a066c062a1ccc0c85794c7b94700a69728231
Veng-Eang <oengvengeang@gmail.com>
2024-02-29 10:34:07 -0500

    add tabs screen

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  Widget activePage = const CategoriesScreen();
+  var activePageTitle = 'Categories';
+
+  void _selectePage(int index) {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectePage,
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
