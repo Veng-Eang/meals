4930d3121bdab5776d85a67b9a179051933d225c
Veng-Eang <oengvengeang@gmail.com>
2024-03-12 22:38:53 -0500

    add and using implicit animate

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -26,7 +26,21 @@ class MealDetailScreen extends ConsumerWidget {
                   content: Text(
                       wasAdd ? 'Meal added as a favorite.' : 'Meal removed.')));
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_outline),
+            icon: AnimatedSwitcher(
+              duration: const Duration(
+                milliseconds: 300,
+              ),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.3, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_outline,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
         title: Text(
@@ -36,11 +50,14 @@ class MealDetailScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 14),
             Text(
